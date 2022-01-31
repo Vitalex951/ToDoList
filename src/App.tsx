67133a4837000589214d5bfc185fbc966728fc80
Vitalex <vitalex951@gmail.com>
2022-01-31 22:50:10 +0300

    'practic1'

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import './App.css';
+import TodoList, {TaskType} from "./Components/TodoList";
 
 function App() {
+    const tasks_1: Array<TaskType> = [
+        {id :1, title: 'HTML', isDone: true},
+        {id :2, title: 'CSS', isDone: true},
+        {id :3, title: 'JS/TS', isDone: true},
+    ]
+
+    const tasks_2: Array<TaskType> = [
+        {id :1, title: 'HTML', isDone: true},
+        {id :2, title: 'CSS', isDone: false},
+        {id :3, title: 'JS/TS', isDone: true},
+    ]
+
+    const tasks_3: Array<TaskType> = [
+            {id :1, title: 'HTML', isDone: false},
+            {id :2, title: 'CSS', isDone: false},
+            {id :3, title: 'JS/TS', isDone: true},
+        ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList  title={'What to learn'} task={tasks_1}/>
+            <TodoList title={'What is ready'} task={tasks_2}/>
+            <TodoList title={'What is buy'} task={tasks_3}/>
         </div>
     );
 }
+
 
 export default App;
---
+++ src/Components/ToDoListHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ToDoListHeaderPropsType = {
+    title: string
+}
+
+const ToDoListHeader = (props: ToDoListHeaderPropsType) => {
+    return <h3>{props.title}</h3>
+};
+
+export default ToDoListHeader;
